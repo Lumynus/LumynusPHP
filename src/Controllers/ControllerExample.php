5b2556df63c6f213408afe9e038ea1fbd34e05c9
Weleny <welenysantos@gmail.com>
2025-12-17 13:53:35 -0300

    main]

--- src/Controllers/ControllerExample.php
+++ src/Controllers/ControllerExample.php
@@ -2,20 +2,16 @@
 
 namespace App\Controllers;
 
-
 use Lumynus\Bundle\Framework\LumynusController;
-use Lumynus\Bundle\Framework\Response;
 use Lumynus\Bundle\Framework\Route;
 
 class ControllerExample extends LumynusController
 {
 
-    #[Route('/exemplo', methods: ['GET'])]
     public function index($req,mixed $a = 'oi')
     {
 
         var_dump(Route::listRoutes());
-       
-
+    
     }
 }
