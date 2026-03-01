330932c7328efbfe98ef43995dcd22df0d6c58ee
Weleny <welenysantos@gmail.com>
2026-03-01 12:49:21 -0300

    main

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'lumynus/lumynus',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '24d118e24e4d17417d320806c900749bf0563b39',
+        'reference' => '082eeff46947fb8fc30b58d63ea0919d7ab531b8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'lumynus/kernel' => array(
             'pretty_version' => 'v1.6.3',
             'version' => '1.6.3.0',
-            'reference' => '362a9d0bfbb7be4bf94f3364c4c4619bd86af6a2',
+            'reference' => 'c55b691895296ebb75ee14ab2e829377579335c3',
             'type' => 'library',
             'install_path' => __DIR__ . '/../lumynus/kernel',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'lumynus/lumynus' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '24d118e24e4d17417d320806c900749bf0563b39',
+            'reference' => '082eeff46947fb8fc30b58d63ea0919d7ab531b8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
